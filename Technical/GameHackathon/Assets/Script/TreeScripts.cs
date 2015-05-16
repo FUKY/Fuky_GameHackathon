07c3f56086db7ce359fdc2e27082cfaf446d04da
tobi994 <viettt.fuky@gmail.com>
2015-05-17 00:47:41 +0700

    Commit Tree lan 4

--- Technical/GameHackathon/Assets/Script/TreeScripts.cs
+++ Technical/GameHackathon/Assets/Script/TreeScripts.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class TreeScripts : MonoBehaviour {
 
+    // animator
     Animator anim;
+
+    // Lượng nước hiện tại của Cây
+    public float waterCurrent = 0.0f;
 
 	// Use this for initialization
 	void Start () {  
@@ -12,8 +16,29 @@ public class TreeScripts : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (waterCurrent >= 1)
+        {
+            Debug.Log("You Win !!");
+        }
 	}
+
+    public void UpdateWaterOfTree(float water)
+    {
+        // lượng nước mà player còn lại đê tưới cây
+        float waterOfPlayer = water * 0.2f;
+
+        waterCurrent += waterOfPlayer;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            UpdateWaterOfTree(Demo_Player.water);
+            Destroy(col.gameObject);
+        }
+    }
+
 
     public void SetAnimationStand1()
     {
@@ -36,5 +61,6 @@ public class TreeScripts : MonoBehaviour {
         anim.SetTrigger("stand5");
     }
 
+    
 
 }
