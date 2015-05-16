4405feba2d4325345b2e654d32fefea8fe505be9
tobi994 <viettt.fuky@gmail.com>
2015-05-16 16:10:15 +0700

    commit item

---
+++ Technical/GameHackathon/Assets/Asset_Tobi/scrips/ClockScrips.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockScrips : MonoBehaviour {
+    
+    // biến dừng vật phẩm trên trời
+    public bool isStop = false;
+    // list sprite của item 
+    // Random khi Item được tạo ra 
+    public Sprite[] listSpriteItem;
+
+    public int[] listKey;
+
+    private SpriteRenderer spriteOfItem;
+
+    public Rigidbody2D rigi;
+
+    void Awake()
+    {
+        spriteOfItem = GetComponent<SpriteRenderer>();
+
+        int keyCurrent = Random.Range(0, listKey.Length);
+
+        spriteOfItem.sprite = listSpriteItem[keyCurrent];
+
+        rigi = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isStop = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Technical/GameHackathon/Assets/Asset_Tobi/scrips/SpawnItem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnItem : MonoBehaviour {
+
+    private Vector3 _startPosition;
+
+    // biến dừng thả item nếu nhận được itemSupport
+    public static bool stop = false;
+
+    //khoang cach 2 lan spawn
+    public float distanceTime = 3.0f;
+
+    float _startTime = 0.0f;
+    float _subTime = 0.0f;
+
+    //vị trí min random item
+    public int minRandom;
+
+    // vị trí max random item
+    public int maxRandom;
+
+    // item 
+    public GameObject item;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        _startPosition = transform.position;
+        GetComponent<Rigidbody2D>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        _subTime = Time.time - _startTime;
+
+        if (_subTime > distanceTime )
+        {
+            // random vị trí x của spawnItem
+            int newPosX = Random.Range(minRandom, maxRandom);
+            // vị trí mới thả item
+            Vector3 newPos = new Vector3(newPosX, transform.position.y,transform.position.z);
+            
+            transform.position = newPos;
+
+            Instantiate(item, transform.position, Quaternion.identity);
+
+            _startTime = Time.time;
+        }
+	}
+}
